d0194452591e45f8d6af7d6b6cfc01b63764f4ef
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2022-07-09 03:48:57 +0100

    udagram deployment setup

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=dbadmin
+export POSTGRES_PASSWORD=Mathematics1992
+export POSTGRES_HOST=postgresdb.cpne23ntxxhv.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_BUCKET=arn:aws:s3:::nkorojoseph-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=Profile
 export JWT_SECRET=testing
 export URL=http://localhost:8100
