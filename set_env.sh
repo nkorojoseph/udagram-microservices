ea5076b6f0a20a27277b4fc39fa8b8a883b71495
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2022-07-12 22:29:43 +0100

    deployment file

--- set_env.sh
+++ set_env.sh
@@ -6,6 +6,6 @@ export POSTGRES_HOST=postgresdb.cpne23ntxxhv.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::nkorojoseph-dev
 export AWS_REGION=us-east-2
-export AWS_PROFILE=Profile
+export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
