692c62bc1c4ef3b88d3e13c0abd74b0449cb3240
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2022-07-18 23:07:28 +0100

    env reading

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -8,7 +8,7 @@ import bodyParser from 'body-parser';
 import {config} from './config/config';
 import {V0_USER_MODELS} from './controllers/v0/model.index';
 
-
+ 
 (async () => {
   await sequelize.addModels(V0_USER_MODELS);
 
