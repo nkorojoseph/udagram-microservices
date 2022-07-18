2a67580d2c8eec4e07e59418c872c6a72e8f80d6
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2022-07-19 00:15:34 +0100

    updated frontend image

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af8380faafeee4591b2f6ff1309ed949-916394287.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
