a72ac6475bcb8e708a3eee88eea3e83fa2fba11a
Nkoro Joseph <nkorojoseph@yahoo.com>
2023-02-05 01:39:31 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=dbadmin
-export POSTGRES_PASSWORD=Mathematics1992
-export POSTGRES_HOST=postgresdb.cpne23ntxxhv.us-east-2.rds.amazonaws.com
+export POSTGRES_USERNAME= 
+export POSTGRES_PASSWORD= 
+export POSTGRES_HOST= 
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::nkorojoseph-dev
+export AWS_BUCKET=arn:aws:s3::: 
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
